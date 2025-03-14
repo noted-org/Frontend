4c5dd983220c1661c96aee54a6768cfeb1a91c6a
emma <emma.sophie8@web.de>
2025-03-14 16:26:05 +0100

    Navbar eingerichtet und grobes routing zu den Seiten. Ohne irgendwas wird man momentan auf die Login Seite weitergeleitet. Styling könnte später noch in globales Styling ausgelagert werden.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  private router = inject(Router);
+
+  goToHome(): void{
+    this.userId = localStorage.getItem('_id');
+    console.log("clicked");
+    this.router.navigate([`/home/${this.userId}`]);
+  }
+
+  userId: String | null = null;
+  goToProfile(): void{
+    this.userId = localStorage.getItem('_id');
+
+    this.router.navigate([`/profile/${this.userId}`]).then(()=> {
+      window.location.reload();
+    });
+  }
+
+  logout(): void{
+    localStorage.removeItem('_id');
+    this.router.navigate([`/login`]).then(()=> {
+      window.location.reload();
+    });
+  }
 
 }
