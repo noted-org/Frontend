29c96b185fa9954e9a3cb4d41b7af1d93a0e3da1
emma <emma.sophie8@web.de>
2025-04-06 16:25:55 +0200

    Flexible Anzeige der Buttons unter dem Menü, jenachdem ob der Nutzer eingeloggt is.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,25 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   private router = inject(Router);
+
+  loggedIn: boolean = false;
+
+  ngOnInit(): void{
+    this.loggedIn = !!localStorage.getItem('id');
+  }
 
   goToHome(): void{
     this.userId = localStorage.getItem('id');
@@ -28,10 +35,20 @@ export class HeaderComponent {
       window.location.reload();
     });
   }
+  goToLogin(): void{
+    this.router.navigate([`/login`]).then(()=> {
+      window.location.reload();
+    });
+  }
+  goToRegister(): void{
+    this.router.navigate([`/register`]).then(()=> {
+      window.location.reload();
+    });
+  }
 
   logout(): void{
     localStorage.removeItem('id');
-    this.router.navigate([`/login`]).then(()=> {
+    this.router.navigate([``]).then(()=> {
       window.location.reload();
     });
   }
